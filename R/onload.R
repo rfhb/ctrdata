963310717525d17f5b5a371ae33559b723ed449c
Ralf Herold <ralf.herold@gmx.net>
2015-09-23 12:54:42 +0100

    strengthened startup message

--- R/onload.R
+++ R/onload.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
 
-  packageStartupMessage ("Please respect the requirements of the copyrights of \nthe clinical trial registers when using their information, \nsee openCTRWebBrowser(copyright = TRUE).\nMore information on this package: https://github.com/rfhb/ctrdata")
+  packageStartupMessage ("\nPlease respect the requirements of the copyrights of \nthe clinical trial registers when using their information, \nsee openCTRWebBrowser(copyright = TRUE).\n\nMore information on this package and how to use it: https://github.com/rfhb/ctrdata/\n")
 
   invisible()
 }
