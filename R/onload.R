a2fb84565f4adfed2545cc24a1ff291cb67aa9d5
Ralf Herold <ralf.herold@gmx.net>
2015-09-25 12:39:20 +0100

    update version and migration hints

--- R/onload.R
+++ R/onload.R
@@ -1,6 +1,10 @@
 .onAttach <- function(libname, pkgname) {
 
-  packageStartupMessage ("\nPlease respect the requirements of the copyrights of \nthe clinical trial registers when using their information, \nsee openCTRWebBrowser(copyright = TRUE).\n\nMore information on this package and how to use it: https://github.com/rfhb/ctrdata/\n")
+  packageStartupMessage("\nPlease respect the requirements of the copyrights of \n",
+                        "the clinical trial registers when using their information, \n",
+                        "see openCTRWebBrowser(copyright = TRUE).\n\n",
+                        "More on this package and how to use it: https://github.com/rfhb/ctrdata/\n\n",
+                        "Note for this version: function findCTRkey was renamed to dbFindCTRkey.\n")
 
   invisible()
 }
