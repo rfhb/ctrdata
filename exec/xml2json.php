13bf0b4fc57ac517d0998d74215f5f84582d0d3e
Ralf Herold <ralf.herold@gmx.net>
2015-10-08 11:33:00 +0100

    avoid unlinking error and improve whitespace handling

--- exec/xml2json.php
+++ exec/xml2json.php
@@ -14,28 +14,31 @@ file_exists($testXmlFile) or die('Directory or file does not exist: ' . $testXml
 
 // determine outfile and make sure it does not exist
 $outFileName = $testXmlFile . '/allfiles.json';
-unlink($outFileName);
+file_exists($outFileName) and unlink($outFileName);
 
 foreach (glob("$testXmlFile/NCT*.xml") as $inFileName) {
 
   $fileContents = file_get_contents($inFileName);
+
+  // normalise contents and remove whitespace
+  $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
+  $fileContents = preg_replace('/ +/', ' ', $fileContents);
+  $fileContents = trim(str_replace('"', "'", $fileContents));
 
   // write NCT number into _id for respective study
   // may break and approach can be improved
   //<id_info><nct_id>NCT00097292</nct_id></id_info>
   $fileContents = str_replace('<required_header>', '<_id>' . basename($inFileName, '.xml') . '</_id><required_header>', $fileContents);
 
-  $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
-  $fileContents = trim(str_replace('"', "'", $fileContents));
+  // turn repeat elements
 
-  $simpleXml = simplexml_load_string($fileContents);
+  $simpleXml = simplexml_load_string($fileContents, 'SimpleXMLElement', LIBXML_COMPACT | LIBXML_NOBLANKS | LIBXML_NOENT);
   // to also flatten cdata elements:
   //$simpleXml = simplexml_load_string($fileContents,'SimpleXMLElement',LIBXML_NOCDATA)
 
-  // Json & Array from XML in 3 lines:
-  //$xml = simplexml_load_string($xml_string);
-  //$json = json_encode($xml);
-  //$array = json_decode($json,TRUE);
+  // Json & Array from XML:
+  //$json = json_encode($simpleXml);
+  //$simpleXml = json_decode($json, TRUE);
 
   // save all in one concatenated file
   file_put_contents($outFileName, json_encode($simpleXml), FILE_APPEND | LOCK_EX);
