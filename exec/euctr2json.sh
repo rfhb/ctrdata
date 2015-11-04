0f80510e77cdbf6803fb1bdd07c907e926cf3f89
Ralf Herold <ralf.herold@gmx.net>
2015-11-04 23:05:28 +0000

    change sponsor into array

--- exec/euctr2json.sh
+++ exec/euctr2json.sh
@@ -20,6 +20,7 @@ LC_CTYPE=C && LANG=C && < "$1/allfiles.txt" perl -ne '
   next if /^$/;
   next if /^Summary$/;
   next if /^This file contains .*$/;
+  next if /Sponsor Information/;
 
   # remove explanatory information from key F.3.3.1
   next if /^\(For clinical trials recorded/;
@@ -30,7 +31,8 @@ LC_CTYPE=C && LANG=C && < "$1/allfiles.txt" perl -ne '
 
   # add identifiers for special cases
   s/^(EudraCT Number.*)$/X.1 $1/g;
-  s/^(Sponsor.*)$/X.2 $1/g;
+  s/^.*(Protocol Code Number.*)$/X.2 Sponsor $1/g;
+  s/^(Sponsor) (.*)$/B.1 $1: $2/g;
   s/^(National Competent.*)$/X.3 $1/g;
   s/^(Clinical Trial.*)$/X.4 $1/g;
   s/^(Trial Status.*)$/X.5 $1/g;
@@ -44,12 +46,13 @@ LC_CTYPE=C && LANG=C && < "$1/allfiles.txt" perl -ne '
   s/\"|\{|\}//g;
 
   # handle single case where colon is in key
-  s/^(.+)Opinion: Reason(.+)$/\1Opinion Reason\2/g;
+  s/^(.+)Opinion: Reason(.+)$/$1Opinion Reason$2/g;
 
   # create id per record from eudract number + country 2 character id
   s/^X.7 Link.*search\/trial\/([0-9-]*)\/([A-Z][A-Z])\/$/"xxxxxxxxxx_id": "$1-$2"/g;
 
   # crude attack on newlines within variable fields
+  #s/^([ABDEFGNPX][.][1-9 I].+)$/xxxxxxxxxx$1/g;
   s/^([ABDEFGNPX][.][1-9 I].+)$/\nxxxxxxxxxx$1/g;
   s/\n/ /g;
   s/xxxxxxxxxx/\n/g;
@@ -58,7 +61,7 @@ LC_CTYPE=C && LANG=C && < "$1/allfiles.txt" perl -ne '
 
   ' | \
 perl -pe '
-   # split key and value, delete special characters, transform, trim, construct quuoted key value pair
+   # split key and value, delete special characters, transform, trim, construct quoted key value pair
    s/^(.+?): (.*)$/ my ($tmp1, $tmp2) = ( lc ($1), $2);
       $tmp1 =~ s! !_!g;
       $tmp1 =~ s![^a-z0-9_]!!g ;
@@ -69,6 +72,7 @@ perl -pe '
 sed \
   -e 's/\("x1_eudract_number.*$\)/}{\1/g' \
   -e 's/^"dimp": "1",$/"dimp": [ { "_dimp": "1",/g' \
+  -e 's/^"b1_sponsor": "1",$/"b1_sponsor": [ { "_b1_sponsor": "1",/g' \
   -e '/^["{}]/!d' \
   -e '/""/d' \
   | \
@@ -80,9 +84,14 @@ perl -pe 'BEGIN{undef $/;}
   # delete comma from last line in record
   s/,\n}{/}\n\n{/g ;
 
-  # now create array with imp(s)
+  # create array with imp(s)
   s/("d[0-9]+_.*"),\n"dimp": "([2-9])",/$1}, \n{ "_dimp": "$2",/g ;
   s/("d[0-9]+_.*"),\n"e1(.*)/$1}\n],\n"e1$2/g ;
+
+  # create array with sponsor(s), closed before dimp or e11_ elements
+  s/("b[0-9]+_.*"),\n"b1_sponsor": "([2-9])",/$1}, \n{ "_b1_sponsor": "$2",/g ;
+  s/("b[0-9]+_.*"),\n"dimp(.*)/$1}\n],\n"dimp$2/g ;
+  s/("b[0-9]+_.*"),\n"e11_medical(.*)/$1}\n],\n"e11_medical$2/g ;
 
   ' \
 > "$1/allfiles.json"
