9af75be1f40227741644dd1cae07c0eeca184a40
Ralf Herold <ralf.herold@gmx.net>
2016-12-15 13:33:03 +0000

    only test binaries after installation

--- R/onload.R
+++ R/onload.R
@@ -14,14 +14,20 @@ assign("mongoBinaryLocation", NA, envir = .privateEnv)
                         "More on this package and how to use it: https://github.com/rfhb/ctrdata/\n"
   )
   #
-  # check availabilities
-  if (.Platform$OS.type == "windows") installCygwinWindowsTest()
+  # check availabilities if already installed
+  tmp <- try(find.package("ctrdata"), silent = TRUE)
   #
-  if(!suppressWarnings(installFindBinary("php --version")))                         warning("php not found, ctrLoadQueryIntoDb() will not work.", call. = FALSE)
-  if(!suppressWarnings(installFindBinary("php -r 'simplexml_load_string(\"\");'"))) warning("php xml not found, ctrLoadQueryIntoDb() will not work.", call. = FALSE)
-  if(!suppressWarnings(installFindBinary("echo x | sed s/x/y/")))                   warning("sed not found, ctrLoadQueryIntoDb() will not work.", call. = FALSE)
-  if(!suppressWarnings(installFindBinary("perl -V:osname")))                        warning("perl not found, ctrLoadQueryIntoDb() will not work.", call. = FALSE)
-  packageStartupMessage("Helper binaries tested.")
+  if(class(tmp) != "try-error") {
+    #
+    if (.Platform$OS.type == "windows") installCygwinWindowsTest()
+    #
+    if(!suppressWarnings(installFindBinary("php --version")))                         warning("php not found, ctrLoadQueryIntoDb() will not work.", call. = FALSE)
+    if(!suppressWarnings(installFindBinary("php -r 'simplexml_load_string(\"\");'"))) warning("php xml not found, ctrLoadQueryIntoDb() will not work.", call. = FALSE)
+    if(!suppressWarnings(installFindBinary("echo x | sed s/x/y/")))                   warning("sed not found, ctrLoadQueryIntoDb() will not work.", call. = FALSE)
+    if(!suppressWarnings(installFindBinary("perl -V:osname")))                        warning("perl not found, ctrLoadQueryIntoDb() will not work.", call. = FALSE)
+    packageStartupMessage("Helper binaries tested.")
+    #
+  }
   #
   invisible()
   #
