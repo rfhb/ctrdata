c4ab2fa5f9271d6f35fbf461ae97ba334391e0b9
Ralf Herold <ralf.herold@gmx.net>
2019-10-06 19:55:38 +0200

    testing correction

--- inst/tinytest/ctrdata_euctr.R
+++ inst/tinytest/ctrdata_euctr.R
@@ -26,9 +26,11 @@ expect_true(tmp_test$n > 10L,
             info = "ctrdata_euctr.R#22")
 
 # test
-expect_true(all(c("2007-000371-42-FR", "2010-019340-40-GB", "2010-019340-40-3RD")
-                %in% tmp_test$success),
-            info = "ctrdata_euctr.R#27")
+expect_true(all(
+  c("2007-000371-42-FR", "2010-019340-40-GB",
+    "2010-019340-40-3RD") %in%
+    tmp_test$success),
+  info = "ctrdata_euctr.R#27")
 
 # test
 expect_true(length(tmp_test$failed) == 0L,
@@ -40,7 +42,7 @@ expect_true(length(tmp_test$failed) == 0L,
 # query based on date is used since this avoids no trials are found
 
 date.today <- Sys.time()
-date.from  <- format(date.today - (60 * 60 * 24 * 12), "%Y-%m-%d")
+date.from  <- format(date.today - (60 * 60 * 24 * 15), "%Y-%m-%d")
 date.to    <- format(date.today - (60 * 60 * 24 *  9), "%Y-%m-%d")
 
 q <- paste0("https://www.clinicaltrialsregister.eu/ctr-search/search?query=",
@@ -96,50 +98,52 @@ expect_true(length(tmp_test$failed) == 0L,
 
 #### ctrLoadQueryIntoDb results ####
 
+# https://www.clinicaltrialsregister.eu/ctr-search/search?query=&age=under-18&phase=phase-three&resultsstatus=trials-with-results
+
 # get trials with results
 q <- paste0("https://www.clinicaltrialsregister.eu/ctr-search/search?query=",
-            "2007-000371-42+OR+2011-004742-18")
+            "2013-003420-37+OR+2009-011454-17+OR+2006-005357-29")
 
 expect_message(
   suppressWarnings(
     ctrLoadQueryIntoDb(
       queryterm = q,
       euctrresults = TRUE,
+      verbose = TRUE,
       con = dbc)),
   "Imported or updated results for",
   info = "ctrdata_euctr.R#106")
 
-# tmp <- nodbi::docdb_get(src = dbc, key = dbc$collection)
-# tmp <- nodbi::docdb_query(src = dbc, key = dbc$collection,
-#                           query = '{}', listfields = 1L)
+dbFindFields(namepart = c("statistic"),
+             con = dbc)
 
 # get results
 result <- suppressWarnings(
   dbGetFieldsIntoDf(
     fields = c("a2_eudract_number",
-               "trialInformation.primaryCompletionDate",
+               "trialInformation.globalEndOfTrialDate",
+               "p_date_of_the_global_end_of_the_trial",
+               "trialInformation.recruitmentStartDate",
+               "x6_date_on_which_this_record_was_first_entered_in_the_eudract_database",
                "subjectDisposition.postAssignmentPeriods.postAssignmentPeriod.arms.arm",
-               "endPoints.endPoint.statisticalAnalyses.statisticalAnalysis.title",
-               "endPoints.endPoint.armReportingGroups.armReportingGroup",
-               "endPoints.endPoint.type.value" ,
                "subjectDisposition.postAssignmentPeriods.postAssignmentPeriod.arms.arm.type.value",
-               "endPoints.endPoint",
+               "endPoints.endPoint", # this is a list
                "trialInformation.analysisForPrimaryCompletion",
-               "firstreceived_results_date",
-               "e71_human_pharmacology_phase_i",
-               "version_results_history"),
+               "e71_human_pharmacology_phase_i"
+    ),
     con = dbc,
-    stopifnodata = FALSE))
+    stopifnodata = FALSE)
+)
 
 # keep only one record for trial
 result <- suppressWarnings(suppressMessages(
-  result[ result[["_id"]]
-          %in% dbFindIdsUniqueTrials(con = dbc), ]
+  result[ result[["_id"]] %in%
+            dbFindIdsUniqueTrials(con = dbc), ]
 ))
 
 # test
-expect_true(all(as.Date(c("2016-07-28"))
-                %in% result$firstreceived_results_date),
+expect_true(all(as.Date(c("2013-10-28", "2018-03-13")) %in%
+                  result$trialInformation.globalEndOfTrialDate),
             info = "ctrdata_euctr.R#138")
 
 # test
@@ -149,53 +153,48 @@ expect_true("logical" == class(result[[
 
 # test
 expect_true("Date" == class(result[[
-  "trialInformation.primaryCompletionDate"]]),
+  "trialInformation.globalEndOfTrialDate"]]),
   info = "ctrdata_euctr.R#149")
 
 # test
 expect_true(
-  sum(nchar(getSublistKey(
-    result,
-    list(
-      c("subjectDisposition.postAssignmentPeriods.postAssignmentPeriod.arms.arm",
-        "title"))
-  )),
-  na.rm = TRUE)
-  > 200L,
+  sum(nchar(
+    getSublistKey(
+      result,
+      list(
+        c("subjectDisposition.postAssignmentPeriods.postAssignmentPeriod.arms.arm",
+          "title"))
+  )
+  ), na.rm = TRUE)
+  > 300L,
   info = "ctrdata_euctr.R#161")
 
 # test
 expect_true(
-  sum(nchar(getSublistKey(
+  sum(nchar(
+    getSublistKey(
     result,
     list(
       c("endPoints.endPoint", "title"))
-  )),
-  na.rm = TRUE)
-  > 2000L,
+  )
+  ), na.rm = TRUE)
+  > 4000L,
   info = "ctrdata_euctr.R#172")
 
 # test
-# tmp2 <- unlist(result[["endPoints.endPoint"]], recursive = TRUE)
-# getNames(tmp2)
 tmp_test <- getSublistKey(
   fulllist = result,
   keyssublists =
     list(
       c("endPoints.endPoint", "^title"),
-      c("endPoints.endPoint", "statisticalAnalyses.statisticalAnalysis.statisticalHypothesisTest.value[0-9]*$"),
-      c("endPoints.endPoint", "statisticalAnalyses.statisticalAnalysis.title[0-9]*$"),
       c("endPoints.endPoint", "^type.value")
     )
 )
-expect_true(all(tmp_test$endPoints.endPoint.type.value %in%
-                  c("ENDPOINT_TYPE.primary", "ENDPOINT_TYPE.secondary", NA)),
-            info = "ctrdata_euctr.R#189")
-
-# extractKey(tmp2, "^armReportingGroups.armReportingGroup.tendencyValues.tendencyValue.value")
-# extractKey(tmp2, "armReportingGroups.armReportingGroup.subjects")
-# extractKey(tmp2, "armReportingGroups.armReportingGroup.@attributes.id")
-# extractKey(tmp2, "armReportingGroups.armReportingGroup.comment")
+expect_true(all(
+  tmp_test$endPoints.endPoint.type.value %in%
+    c("ENDPOINT_TYPE.primary", "ENDPOINT_TYPE.secondary",
+      "ENDPOINT_TYPE.other", NA)),
+  info = "ctrdata_euctr.R#189")
 
 # test
 tmp_test <- getSublistKey(
@@ -211,8 +210,31 @@ expect_true(
   sum(
     as.numeric(
       tmp_test[["endPoints.endPoint.armReportingGroups.armReportingGroup.subjects"]]),
-    na.rm = TRUE) > 1250L,
+    na.rm = TRUE) > 2000L,
   info = "ctrdata_euctr.R#211")
+
+# test
+tmp_test <- getSublistKey(
+  fulllist = result,
+  keyssublists =
+    list(
+      c("endPoints.endPoint", "statisticalAnalyses.statisticalAnalysis.title"),
+      c("endPoints.endPoint", "statisticalAnalyses.statisticalAnalysis.statisticalHypothesisTest.method.value"),
+      c("endPoints.endPoint", "statisticalAnalyses.statisticalAnalysis.statisticalHypothesisTest.value[0-9]*$")
+    )
+)
+expect_true(
+  all(na.omit(
+    as.numeric(
+      tmp_test[["endPoints.endPoint.statisticalAnalyses.statisticalAnalysis.statisticalHypothesisTest.value"]]
+    )) < 1L),
+  info = "ctrdata_euctr.R#211")
+expect_true(all(
+    c("HYPOTHESIS_METHOD.cochranMantelHaenszel", "HYPOTHESIS_METHOD.ancova",
+      "HYPOTHESIS_METHOD.regressionLogistic") %in%
+      tmp_test$endPoints.endPoint.statisticalAnalyses.statisticalAnalysis.statisticalHypothesisTest.method.value),
+  info = "ctrdata_euctr.R#241")
+
 
 #### dbFindFields #####
 
@@ -246,7 +268,7 @@ tmp_test <- suppressMessages(suppressWarnings(
     con = dbc)))
 expect_true("character" %in% class(tmp_test),
             info = "ctrdata_euctr.R#243")
-expect_true(length(tmp_test) >= 5L,
+expect_true(length(tmp_test) >= 4L,
             info = "ctrdata_euctr.R#246")
 
 #### dbFindIdsUniqueTrials #####
@@ -281,8 +303,8 @@ expect_warning(
 
 # test
 expect_true(all(
-  c("2007-000371-42-PL",  "2011-004742-18-PL")
-  %in% tmp_test),
+  c("2007-000371-42-PL", "2017-003455-35-3RD") %in%
+    tmp_test),
   info = "ctrdata_euctr.R#283")
 
 #### annotations #####
