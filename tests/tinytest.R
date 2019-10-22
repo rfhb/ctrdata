4f39c018cbd81735a84ddc2e8791e17e2b1d4b43
Ralf Herold <ralf.herold@gmx.net>
2019-10-22 22:53:20 +0200

    tinytest check for at_home to avoid issues on CRAN

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,5 +1,7 @@
 
-if ( requireNamespace("tinytest", quietly=TRUE) ){
-  tinytest::test_package("ctrdata")
-}
+if ( requireNamespace("tinytest", quietly = TRUE) ) {
 
+  # at_home is for development version e.g. 0.20.1.9001
+  home <- length(unclass(packageVersion("ctrdata"))[[1]]) == 4
+  tinytest::test_package("ctrdata", at_home = home)
+}
