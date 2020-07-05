a3005c30a3cdd0833f46611591b3d19e6112e6e0
Ralf Herold <ralf.herold@gmx.net>
2020-07-05 22:46:57 +0200

    revert to check env variable for tinytest at home

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -2,7 +2,7 @@
 if (requireNamespace("tinytest", quietly = TRUE) &&
     utils::packageVersion("tinytest") >= "1.0.0") {
 
-  home <- length(unclass(packageVersion("ctrdata"))[[1]]) == 4
-  # home <- Sys.getenv("ATHOME") != ""
+  # home <- length(unclass(packageVersion("ctrdata"))[[1]]) == 4
+  home <- Sys.getenv("ATHOME") != ""
   tinytest::test_package("ctrdata", at_home = home)
 }
