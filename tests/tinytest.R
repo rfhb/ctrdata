8162e0b19a95886b8c87fb772a5a5175c723cf7e
Ralf Herold <ralf.herold@gmx.net>
2020-07-05 20:34:11 +0200

    attempt to run tinytests on ci services

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,8 +1,8 @@
-
-if ( requireNamespace("tinytest", quietly = TRUE) &&
-     utils::packageVersion("tinytest") >= "1.0.0") {
+# see section 4.4 in vignette("using_tinytest", "tinytest")
+if (requireNamespace("tinytest", quietly = TRUE) &&
+    utils::packageVersion("tinytest") >= "1.0.0") {
 
   # home <- length(unclass(packageVersion("ctrdata"))[[1]]) == 4
-  home <- Sys.getenv("AT_HOME") != ""
+  home <- Sys.getenv("ATHOME") != ""
   tinytest::test_package("ctrdata", at_home = home)
 }
