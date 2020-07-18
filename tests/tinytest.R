03f3a63fa4dcfb4a51648e611b4de9a6d17c29ec
Ralf Herold <ralf.herold@gmx.net>
2020-07-18 16:48:24 +0200

    add creating artifact for checking which tests were run

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,8 +1,25 @@
-# see section 4.4 in vignette("using_tinytest", "tinytest")
 if (requireNamespace("tinytest", quietly = TRUE) &&
     utils::packageVersion("tinytest") >= "1.0.0") {
 
-  home <- length(unclass(packageVersion("ctrdata"))[[1]]) == 4
-  # home <- Sys.getenv("ATHOME") != ""
-  tinytest::test_package("ctrdata", at_home = home)
+  # extended testing (at_home = TRUE) for CI
+  # but not to be executed on CRAN (where
+  # released versions have 3 components)
+
+  # - see section 4.4 in vignette("using_tinytest", "tinytest")
+  cran <- !length(unclass(packageVersion("ctrdata"))[[1]]) == 4
+  # - alternative criterion
+  # cran <- Sys.getenv("ATHOME") == ""
+
+  # do testing
+  tt <- tinytest::test_package(
+    pkgname = "ctrdata",
+    at_home = cran)
+
+  # unless on CRAN
+  if (!cran) {
+    saveRDS(
+      object = tt,
+      file = "tinytest_value.rds")
+  }
+
 }
