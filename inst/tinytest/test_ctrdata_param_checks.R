9ea37d87fd3ab8bf77b031db2620d7645d41897b
Ralf Herold <ralf.herold@gmx.net>
2020-07-22 20:40:33 +0200

    update workflow dependencies

--- inst/tinytest/test_ctrdata_param_checks.R
+++ inst/tinytest/test_ctrdata_param_checks.R
@@ -14,64 +14,73 @@ dbc <- nodbi::src_sqlite(
 tmpdf <- iris[1:5,]
 names(tmpdf) <- paste0("query-", names(tmpdf))
 expect_error(
-  ctrLoadQueryIntoDb(
-    queryterm = tmpdf,
-    con = dbc))
+  suppressWarnings(
+    ctrLoadQueryIntoDb(
+      queryterm = tmpdf,
+      con = dbc)))
 expect_error(
-  ctrLoadQueryIntoDb(
-    queryterm = tmpdf,
-    querytoupdate = 1L,
-    con = dbc))
+  suppressWarnings(
+    ctrLoadQueryIntoDb(
+      queryterm = tmpdf,
+      querytoupdate = 1L,
+      con = dbc)))
 tmpdf["query-term"] <- as.character(tmpdf[["query-Species"]])
 expect_error(
-  ctrLoadQueryIntoDb(
-    queryterm = tmpdf,
-    querytoupdate = 1L,
-    con = dbc))
+  suppressWarnings(
+    ctrLoadQueryIntoDb(
+      queryterm = tmpdf,
+      querytoupdate = 1L,
+      con = dbc)))
 
 expect_error(
-  ctrLoadQueryIntoDb(
-    queryterm = "",
-    con = dbc))
+  suppressWarnings(
+    ctrLoadQueryIntoDb(
+      queryterm = "",
+      con = dbc)))
 
 expect_error(
-  ctrLoadQueryIntoDb(
-    querytoupdate = pi,
-    con = dbc))
+  suppressWarnings(
+    ctrLoadQueryIntoDb(
+      querytoupdate = pi,
+      con = dbc)))
 
 expect_error(
-  ctrLoadQueryIntoDb(
-    querytoupdate = "notlast",
-    con = dbc))
+  suppressWarnings(
+    ctrLoadQueryIntoDb(
+      querytoupdate = "notlast",
+      con = dbc)))
 
 # this also checks only.count
 # and that no records were found
 expect_warning(
-  ctrLoadQueryIntoDb(
-    queryterm = "someQueryForErrorTriggering",
-    querytoupdate = 1L,
-    only.count = TRUE,
-    con = dbc),
-  "'queryterm' and 'querytoupdate' specified, continuing")
+  suppressWarnings(
+    ctrLoadQueryIntoDb(
+      queryterm = "someQueryForErrorTriggering",
+      querytoupdate = 1L,
+      only.count = TRUE,
+      con = dbc),
+    "'queryterm' and 'querytoupdate' specified, continuing"))
 
 
 #### database ####
 
 dbc <- nodbi::src_sqlite()
 expect_error(
-  ctrLoadQueryIntoDb(
-    queryterm = "someQueryForErrorTriggering",
-    only.count = TRUE,
-    con = dbc),
-  "parameter 'collection' needs")
+  suppressWarnings(
+    ctrLoadQueryIntoDb(
+      queryterm = "someQueryForErrorTriggering",
+      only.count = TRUE,
+      con = dbc),
+    "parameter 'collection' needs"))
 
 dbc <- nodbi::src_sqlite(
   collection = "inmemory")
 RSQLite::dbDisconnect(conn = dbc$con)
 expect_warning(
-  ctrLoadQueryIntoDb(
-    queryterm = "someQueryForErrorTriggering",
-    only.count = TRUE,
-    con = dbc),
-  "Database connection was closed, trying to reopen")
+  suppressWarnings(
+    ctrLoadQueryIntoDb(
+      queryterm = "someQueryForErrorTriggering",
+      only.count = TRUE,
+      con = dbc),
+    "Database connection was closed, trying to reopen"))
 
