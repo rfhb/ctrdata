85819e31092918e9fa5cf036c7cb74f02469a3fa
Ralf Herold <ralf.herold@gmx.net>
2020-10-16 20:32:16 +0200

    refactored euctr protocol-related download, no more using batching

--- R/main.R
+++ R/main.R
@@ -1225,82 +1225,192 @@ ctrLoadQueryIntoDbEuctr <- function(
   #
   message("done.")
 
-  # calculate batches to get data from all results pages
-  resultsNumBatches <- resultsEuNumPages %/% parallelretrievals
-  resultsNumModulo  <- resultsEuNumPages %%  parallelretrievals
+  ## download all text files from pages
+
+  # inform user
   message("(1/3) Downloading trials (max. ",
-          parallelretrievals, " page[s] in parallel):")
+          parallelretrievals, " page[s] in parallel)...")
+
+  # prepare download and saving
+
+  # get cookies
+  cf <- tempfile(
+    pattern = "cookies_",
+    fileext = ".txt"
+  )
+  h <- curl::new_handle(
+    ssl_verifypeer = FALSE,
+    useragent = "",
+    accept_encoding = "gzip,deflate,zstd,br",
+    cookiejar = cf,
+    cookiefile = cf)
+  tmp <- curl::curl_fetch_memory(
+    url = paste0(queryEuRoot, queryEuType3,
+                 "2008-003606-33", "&page=1", queryEuPost),
+    handle = h)
+  # curl::handle_cookies(h)
+  # inform user about capabilities
+  stime <- curl::handle_data(h)[["times"]][["total"]]
+  sgzip <- curl::parse_headers(tmp$headers)
+  sgzip <- sgzip[grepl("Transfer-Encoding", sgzip)]
+  sgzip <- grepl("gzip|deflate", sgzip)
+  if (length(sgzip) && !sgzip) {
+    message("Note: register server cannot compress data, ",
+    "transfer takes longer, about ", round(stime),
+    "s per page")
+  }
+
+  # create pool for concurrent connections
+  pool <- curl::new_pool(
+    host_con = parallelretrievals)
 
   # progress indicator function
-  cb <- function(req){message(". ", appendLF = FALSE)}
+  pc <- 0
+  cb <- function(req){
+    pc <<- pc + 1
+    message("Pages #: ", pc, "\r", appendLF = FALSE)
+    # Note: Tranfer-Encoding does not provide gzip
+    # print(curl::parse_headers(req$headers))
+    # [1] "HTTP/1.1 200 OK"
+    # [2] "Date: Sun, 11 Oct 2020 20:09:10 GMT"
+    # [3] "Server: "
+    # [4] "Content-Disposition: attachment; filename = trials-full.txt"
+    # [5] "Transfer-Encoding: chunked"
+    # [6] "Content-Type: application/octet-stream"
+    # [7] "Content-Language: en"
+  }
 
-  # iterate over batches of results pages
-  for (i in 1:(resultsNumBatches +
-               ifelse(resultsNumModulo > 0, 1, 0))) {
+  # generate vector with URLs of all pages
+  urls <- unlist(lapply(
+    paste0(queryEuRoot, queryEuType3,
+           queryterm, "&page=", 1:resultsEuNumPages, queryEuPost),
+    utils::URLencode))
+  # print(urls)
 
-    # parallel requests by using startpage:stoppage
-    startpage <- (i - 1) * parallelretrievals + 1
-    stoppage  <- ifelse(i > resultsNumBatches,
-                        startpage + resultsNumModulo,
-                        startpage + parallelretrievals) - 1
-    message(" p ", startpage, "-", stoppage, " ",
-            appendLF = FALSE)
+  # generate vector with file names for saving pages
+  fp <- paste0(
+    tempDir, "/euctr-trials-page_",
+    formatC(1:resultsEuNumPages,
+            digits = 0,
+            width = nchar(resultsEuNumPages),
+            flag = 0),
+    ".txt")
 
-    # download all text files from pages
-    # in current batch into a variable
+  # add URLs and file names into pool
+  tmp <- lapply(
+    # alternate sequence
+    sample(seq_along(urls),
+           size = length(urls),
+           replace = FALSE,
+           prob = NULL),
+    function(x)
+      curl::curl_fetch_multi(
+        url = urls[x],
+        done = cb,
+        pool = pool,
+        data = fp[x],
+        handle = curl::new_handle(
+          ssl_verifypeer = FALSE,
+          useragent = "",
+          accept_encoding = "deflate, gzip",
+          cookiejar = cf,
+          cookiefile = cf
+        )
+      )
+  )
 
-    # prepare download and saving
-    pool <- curl::new_pool(
-      total_con = parallelretrievals,
-      host_con = parallelretrievals)
-    #
-    urls <- unlist(lapply(
-      paste0(queryEuRoot, queryEuType3,
-             queryterm, "&page=", startpage:stoppage, queryEuPost),
-      utils::URLencode))
-    #
-    fp <- paste0(
-      tempDir, "/euctr-trials-page_",
-      formatC(startpage:stoppage,
-              digits = 0,
-              width = nchar(resultsEuNumPages),
-              flag = 0),
-      ".txt")
-    #
-    tmp <- lapply(
-      seq_along(urls),
-      function(x)
-        curl::curl_fetch_multi(
-          url = urls[x],
-          done = cb,
-          pool = pool,
-          data = fp[x],
-          handle = curl::new_handle(
-            ssl_verifypeer = FALSE
-          )
-        ))
+  # do download and saving
+  tmp <- curl::multi_run(
+    pool = pool
+  )
 
-    # do download and saving
-    tmp <- curl::multi_run(
-      pool = pool,
-      poll = length(urls))
+  # check plausibility
+  if (class(tmp) == "try-error") {
+    stop("Download from EUCTR failed; last error: ",
+         class(tmp), call. = FALSE)
+  }
+  if (tmp[["success"]] != resultsEuNumPages) {
+    stop("Download from EUCTR failed; incorrect number of records.",
+         call. = FALSE)
+  }
 
-    # check plausibility
-    if (class(tmp) == "try-error") {
-      stop("Download from EUCTR failed; last error: ",
-           class(tmp), call. = FALSE)
-    }
-    #
-    # if (length(tmp) != (stoppage - startpage + 1))
-    if (tmp[["success"]] != (stoppage - startpage + 1)) {
-      stop("Download from EUCTR failed; incorrect number of records.",
-           call. = FALSE)
-    }
 
-    # clean up large object
-    rm(tmp)
-
-  } # for batch
+  # # calculate batches to get data from all results pages
+  # resultsNumBatches <- resultsEuNumPages %/% parallelretrievals
+  # resultsNumModulo  <- resultsEuNumPages %%  parallelretrievals
+  # message("(1/3) Downloading trials (max. ",
+  #         parallelretrievals, " page[s] in parallel):")
+  #
+  # # progress indicator function
+  # cb <- function(req){message(". ", appendLF = FALSE)}
+  #
+  # # iterate over batches of results pages
+  # for (i in 1:(resultsNumBatches +
+  #              ifelse(resultsNumModulo > 0, 1, 0))) {
+  #
+  #   # parallel requests by using startpage:stoppage
+  #   startpage <- (i - 1) * parallelretrievals + 1
+  #   stoppage  <- ifelse(i > resultsNumBatches,
+  #                       startpage + resultsNumModulo,
+  #                       startpage + parallelretrievals) - 1
+  #   message(" p ", startpage, "-", stoppage, " ",
+  #           appendLF = FALSE)
+  #
+  #   # download all text files from pages
+  #   # in current batch into a variable
+  #
+  #   # prepare download and saving
+  #   pool <- curl::new_pool(
+  #     total_con = parallelretrievals,
+  #     host_con = parallelretrievals)
+  #   #
+  #   urls <- unlist(lapply(
+  #     paste0(queryEuRoot, queryEuType3,
+  #            queryterm, "&page=", startpage:stoppage, queryEuPost),
+  #     utils::URLencode))
+  #   #
+  #   fp <- paste0(
+  #     tempDir, "/euctr-trials-page_",
+  #     formatC(startpage:stoppage,
+  #             digits = 0,
+  #             width = nchar(resultsEuNumPages),
+  #             flag = 0),
+  #     ".txt")
+  #   #
+  #   tmp <- lapply(
+  #     seq_along(urls),
+  #     function(x)
+  #       curl::curl_fetch_multi(
+  #         url = urls[x],
+  #         done = cb,
+  #         pool = pool,
+  #         data = fp[x],
+  #         handle = curl::new_handle(
+  #           ssl_verifypeer = FALSE
+  #         )
+  #       ))
+  #
+  #   # do download and saving
+  #   tmp <- curl::multi_run(
+  #     pool = pool,
+  #     poll = length(urls))
+  #
+  #   # check plausibility
+  #   if (class(tmp) == "try-error") {
+  #     stop("Download from EUCTR failed; last error: ",
+  #          class(tmp), call. = FALSE)
+  #   }
+  #   #
+  #   # if (length(tmp) != (stoppage - startpage + 1))
+  #   if (tmp[["success"]] != (stoppage - startpage + 1)) {
+  #     stop("Download from EUCTR failed; incorrect number of records.",
+  #          call. = FALSE)
+  #   }
+  #
+  #   # clean up large object
+  #   rm(tmp)
+  #
+  # } # for batch
 
   ## compose commands: for external script on
   # all files in temporary directory and for import
