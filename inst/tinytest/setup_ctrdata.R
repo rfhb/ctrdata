1992f2c89cbe88e26d18f36a6e8f5e31b6365a37
Ralf Herold <ralf.herold@gmx.net>
2021-04-24 08:11:37 +0200

    styled files

--- inst/tinytest/setup_ctrdata.R
+++ inst/tinytest/setup_ctrdata.R
@@ -14,23 +14,26 @@ suppressMessages(library(ctrdata))
 
 #### global variables for data bases ####
 
-tmpName <- paste0("ctrdata_test_",
-                  format(Sys.time(),
-                         "%Y%m%d%H%M%S",
-                         tz = "UTC"), "_",
-                  paste0(sample(letters, 6), collapse = ""))
+tmpName <- paste0(
+  "ctrdata_test_",
+  format(Sys.time(),
+    "%Y%m%d%H%M%S",
+    tz = "UTC"
+  ), "_",
+  paste0(sample(letters, 6), collapse = "")
+)
 
 # local mongodb
 mongoLocalRwCollection <- tmpName
-mongoLocalRwDb         <- "users"
+mongoLocalRwDb <- "users"
 
 # remote mongodb read only
 mongoRemoteRoCollection <- "dbperm"
-mongoRemoteRoDb         <- "dbperm"
+mongoRemoteRoDb <- "dbperm"
 
 # remote mongodb read write
 mongoRemoteRwCollection <- tmpName
-mongoRemoteRwDb         <- "users"
+mongoRemoteRwDb <- "users"
 
 # credentials
 
@@ -48,78 +51,75 @@ mongoRemoteRwUrl <-
 #### helper functions system detection ####
 
 checkBinaries <- function() {
-
-    all(ctrdata:::checkBinary())
-
+  all(ctrdata:::checkBinary())
 }
 
 
 checkInternet <- function() {
-
-  tmp <- try({
-    httr::HEAD("www.clinicaltrials.gov", httr::timeout(5))
-    httr::HEAD("www.clinicaltrialsregister.eu", httr::timeout(5))
-  }, silent = TRUE)
+  tmp <- try(
+    {
+      httr::HEAD("www.clinicaltrials.gov", httr::timeout(5))
+      httr::HEAD("www.clinicaltrialsregister.eu", httr::timeout(5))
+    },
+    silent = TRUE
+  )
 
   out <- !("try-error" %in% class(tmp))
   out
-
 }
 
 
 checkSqlite <- function() {
-
   tmp <- try(nodbi::src_sqlite(), silent = TRUE)
 
   out <- all(c("src_sqlite", "docdb_src") %in% class(tmp))
   if (out) RSQLite::dbDisconnect(conn = tmp$con)
   out
-
 }
 
 
 checkMongoLocal <- function() {
-
   tmp <- try(
     nodbi::src_mongo(
       collection = tmpName,
       db = tmpName,
-      url = "mongodb://localhost"),
-    silent = TRUE)
+      url = "mongodb://localhost"
+    ),
+    silent = TRUE
+  )
 
   out <- all(c("src_mongo", "docdb_src") %in% class(tmp))
   if (out) tmp$con$disconnect()
   out
-
 }
 
 
 checkMongoRemoteRo <- function() {
-
   tmp <- try(
     nodbi::src_mongo(
       collection = tmpName,
       db = tmpName,
-      url = mongoRemoteRoUrl),
-    silent = TRUE)
+      url = mongoRemoteRoUrl
+    ),
+    silent = TRUE
+  )
 
   out <- all(c("src_mongo", "docdb_src") %in% class(tmp))
   if (out) tmp$con$disconnect()
   out
-
 }
 
 checkMongoRemoteRw <- function() {
-
   tmp <- try(
     nodbi::src_mongo(
       collection = tmpName,
       db = tmpName,
-      url = mongoRemoteRwUrl),
-    silent = TRUE)
+      url = mongoRemoteRwUrl
+    ),
+    silent = TRUE
+  )
 
   out <- all(c("src_mongo", "docdb_src") %in% class(tmp))
   if (out) tmp$con$disconnect()
   out
-
 }
