8c8dd20ef9a85ca00d9d4b212632d7606255321d
Ralf Herold <ralf.herold@gmx.net>
2021-05-07 16:30:33 +0200

    ctgov2json chunk converted xml files

--- exec/ctgov2json.php
+++ exec/ctgov2json.php
@@ -22,60 +22,71 @@ if ($argc <= 1) {
 
 file_exists($testXmlFile) or die('Directory or file does not exist: ' . $testXmlFile);
 
-// determine outfile and delete if existing
-$outFileName = $testXmlFile . '/allfiles.json';
-file_exists($outFileName) and unlink($outFileName);
-
 // get UTC date, time in format correspondsing to the
 // R default for format methods: "%Y-%m-%d %H:%M:%S"
 $dt = gmdate("Y-m-d H:i:s");
 
-foreach (glob("$testXmlFile/NCT*.xml") as $inFileName) {
+// chunk and trial numbers
+$cn = 0;
+$tn = 0;
 
-  $fileContents = file_get_contents($inFileName);
+foreach (array_chunk(glob("$testXmlFile/NCT*.xml"), 20) as $chunkFileNames) {
 
-  // normalise contents
-  $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
+  $cn = $cn + 1;
 
-  // https://stackoverflow.com/questions/44765194/how-to-parse-invalid-bad-not-well-formed-xml
-  $fileContents = preg_replace('/[^\x{0009}\x{000a}\x{000d}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}]+/u', ' ', $fileContents);
+  foreach ($chunkFileNames as $inFileName) {
 
-  // escapes
-  $fileContents = preg_replace('/ +/', ' ', $fileContents);
-  $fileContents = trim(str_replace("'", " &apos;", $fileContents));
-  $fileContents = trim(str_replace("&", " &amp;", $fileContents));
+    // user info
+    // print $inFileName . PHP_EOL;
 
-  // remove white space
-  $fileContents = preg_replace('/ +/', ' ', $fileContents);
+    $fileContents = file_get_contents($inFileName);
 
-  // use single quotes for xml
-  $fileContents = trim(str_replace('"', "'", $fileContents));
+    // normalise contents
+    $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
 
-  // First write NCT number into _id for respective study:
-  //    <id_info><nct_id>NCT00097292</nct_id></id_info>
-  // Second write date of last import into additional field:
-  //    <record_last_import>2016-06-22 08:35:32</record_last_import>
-  // Third write clinical trial register name into additional field:
-  //    <ctrname>CTGOV</ctrname>
-  $fileContents = str_replace('<required_header>',
-                              '<_id>' . basename($inFileName, '.xml') . '</_id>' .
-                              '<record_last_import>' . $dt . '</record_last_import>' .
-                              '<ctrname>CTGOV</ctrname>' .
-                              '<required_header>', $fileContents);
+    // https://stackoverflow.com/questions/44765194/how-to-parse-invalid-bad-not-well-formed-xml
+    $fileContents = preg_replace('/[^\x{0009}\x{000a}\x{000d}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}]+/u', ' ', $fileContents);
 
-  // turn repeat elements
-  $simpleXml = simplexml_load_string($fileContents, 'SimpleXMLElement', LIBXML_COMPACT | LIBXML_NOBLANKS | LIBXML_NOENT);
+    // escapes
+    $fileContents = preg_replace('/ +/', ' ', $fileContents);
+    $fileContents = trim(str_replace("'", " &apos;", $fileContents));
+    $fileContents = trim(str_replace("&", " &amp;", $fileContents));
 
-  // to also flatten cdata elements:
-  //$simpleXml = simplexml_load_string($fileContents,'SimpleXMLElement',LIBXML_NOCDATA)
+    // remove white space
+    $fileContents = preg_replace('/ +/', ' ', $fileContents);
 
-  // Json & Array from XML:
-  //$json = json_encode($simpleXml);
-  //$simpleXml = json_decode($json, TRUE);
+    // use single quotes for xml
+    $fileContents = trim(str_replace('"', "'", $fileContents));
 
-  // save all in one concatenated file
-  file_put_contents($outFileName, json_encode($simpleXml) . "\n", FILE_APPEND | LOCK_EX);
+    // First write NCT number into _id for respective study:
+    //    <id_info><nct_id>NCT00097292</nct_id></id_info>
+    // Second write date of last import into additional field:
+    //    <record_last_import>2016-06-22 08:35:32</record_last_import>
+    // Third write clinical trial register name into additional field:
+    //    <ctrname>CTGOV</ctrname>
+    $fileContents = str_replace(
+      '<required_header>',
+      '<_id>' . basename($inFileName, '.xml') . '</_id>' .
+        '<record_last_import>' . $dt . '</record_last_import>' .
+        '<ctrname>CTGOV</ctrname>' .
+        '<required_header>',
+      $fileContents
+    );
 
+    // turn repeat elements
+    $simpleXml = simplexml_load_string($fileContents, 'SimpleXMLElement', LIBXML_COMPACT | LIBXML_NOBLANKS | LIBXML_NOENT);
+
+    // to also flatten cdata elements:
+    //$simpleXml = simplexml_load_string($fileContents,'SimpleXMLElement',LIBXML_NOCDATA)
+
+    // Json & Array from XML:
+    //$json = json_encode($simpleXml);
+    //$simpleXml = json_decode($json, TRUE);
+
+    // save all in concatenated file per chunk
+    file_put_contents("$testXmlFile/NCT_" . $cn . ".json", json_encode($simpleXml) . "\n", FILE_APPEND | LOCK_EX);
+    $tn = $tn + 1;
+  }
 }
 
 ?>
