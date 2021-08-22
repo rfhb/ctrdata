138bf86dcb5888fa729b01e6eddff309db70bfdc
Ralf Herold <ralf.herold@gmx.net>
2021-08-22 11:36:32 +0200

    improve checking for cran

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,21 +1,20 @@
-if (requireNamespace("tinytest", quietly = TRUE) &&
-    utils::packageVersion("tinytest") >= "1.0.0") {
+if (requireNamespace("tinytest", quietly = TRUE)) {
 
   # extended testing (at_home = TRUE) for CI
   # but not to be executed on CRAN (where
   # released versions have 3 components)
 
   # see section 4.4 in vignette("using_tinytest", "tinytest")
-  cran <- !length(unclass(packageVersion("ctrdata"))[[1]]) == 4
+  cran <- length(unclass(packageVersion("ctrdata"))[[1]]) == 3
 
-  # do testing
-  tt <- tinytest::test_package(
-    pkgname = "ctrdata",
-    color = FALSE,
-    at_home = !cran)
-
-  # unless on CRAN
+  # do testing unless on CRAN
   if (!cran) {
+
+    tt <- tinytest::test_package(
+      pkgname = "ctrdata",
+      color = FALSE,
+      at_home = !cran)
+
     saveRDS(
       object = tt,
       file = "tinytest.rds")
