4061e0ff5c09e72d0266d6ca29a0f6d80f56aff6
Ralf Herold <ralf.herold@gmx.net>
2025-02-15 09:31:35 +0100

    include ctis1 fields in .numTestArmsSubstances

--- R/vct_numTestArmsSubstances.R
+++ R/vct_numTestArmsSubstances.R
@@ -12,11 +12,11 @@
 #' @importFrom stringdist stringsimmatrix
 #' @importFrom stringi stri_count_fixed stri_split_fixed
 .numTestArmsSubstances <- function(df = NULL) {
-  
+
   # check generic, do not edit
   stopifnot(is.data.frame(df) || is.null(df))
-  
-  
+
+
   #### fields ####
   fldsNeeded <- list(
     "euctr" = c(
@@ -36,13 +36,16 @@
       "interventions.intervention.drugNames"
     ),
     "ctis" = c(
+      # CTIS1
+      "authorizedPartI.productRoleGroupInfos",
+      # CTIS2
       "authorizedApplication.authorizedPartI.productRoleGroupInfos"
     ))
-  
-  
+
+
   #### describe ####
   if (is.null(df)) {
-    
+
     txt <- '
 Calculates the number of active arms with different investigational medicines,
 after excluding comparator, auxiliary and placebo arms / medicines.
@@ -56,62 +59,62 @@ similarity is 0.8 or higher.
 
 Returns an integer.
     '
-    
+
     # generic, do not edit
     fctDescribe(match.call()[[1]], txt, fldsNeeded)
     return(invisible(fldsNeeded))
-    
+
   } # end describe
-  
-  
+
+
   #### calculate ####
-  
+
   # check generic, do not edit
   fctChkFlds(names(df), fldsNeeded)
-  
+
   # helper definitions
   thresholdSimilar <- 0.8
-  
+
   # helper function
   `%>%` <- dplyr::`%>%`
-  
+
   # helper function, to be call per group
   asTestSimilarityArms <- function(x) {
-    
+
     # early exit
     if (is.null(x) || !length(x) || all(is.na(x))) return(NULL)
-    
+
     # normalise
     x <- tolower(x) # TODO removed unlist(x)
-    
+
     # early exit if single arm
     if (length(x) == 1L) return(0L)
-    
+
     # calculate similarities
     t <- stringdist::stringsimmatrix(x, x)
-    
+
     # early exit
     if (ncol(t) < 1L) return(NA)
-    
+
     # extract max similarity of arm n with other arm(s)
     diag(t) <- NA
     if (all(is.na(t))) return(0L)
     apply(t, 2, max, na.rm = TRUE)
-    
+
   } # asTestSimilarityArms
-  
+
   # helper function, to be called per trial
   asTestSimilarityTrial <- function(x) {
-    
+
     # collapse active substances into one string per treatment arm / group
     t <- sapply(x, function(i) paste0(unique(i), collapse = " / "))
-    
+
     # return vector of similarities of treatment arms
     asTestSimilarityArms(t)
-    
+
   } # end asTestSimilarityTrial
-  
-  
+
+
   #### . EUCTR ####
   df %>% dplyr::mutate(
     #
@@ -147,8 +150,8 @@ Returns an integer.
     )
   ) %>%
     dplyr::pull(out) -> df$euctr
-  
-  
+
+
   #### . CTGOV ####
   df %>% dplyr::mutate(
     #
@@ -175,8 +178,8 @@ Returns an integer.
     out = analysis_numDifferentExpArmsInTrial
   ) %>%
     dplyr::pull(out) -> df$ctgov
-  
-  
+
+
   #### . CTGOV2 ####
   df %>% dplyr::mutate(
     #
@@ -201,8 +204,8 @@ Returns an integer.
     out = analysis_numDifferentExpArmsInTrial
   ) %>%
     dplyr::pull(out) -> df$ctgov2
-  
-  
+
+
   #### . ISRCTN ####
   df %>%
     dplyr::mutate(
@@ -236,35 +239,62 @@ Returns an integer.
       )
     ) %>%
     dplyr::pull(out)-> df$isrctn
-  
-  
+
+
   #### . CTIS ####
   df %>% dplyr::mutate(
     #
-    helper_asNamesPerTestGroup = lapply(
+    helper_asNamesPerTestGroupCtis1 = lapply(
+      authorizedPartI.productRoleGroupInfos,
+      function(i) {if (any(names(i) == "products"))
+        lapply(i[["products"]], "[[", "productName")[
+          i[["productRoleName"]] == "Test"] else NA}),
+    #
+    helper_simTestGroupsInTrialCtis1 = lapply(
+      helper_asNamesPerTestGroupCtis1, function(i) asTestSimilarityTrial(i)),
+    #
+    helper_numTestGroupsInTrialCtis1 = sapply(
+      helper_simTestGroupsInTrialCtis1, length, USE.NAMES = FALSE, simplify = TRUE),
+    #
+    analysis_numDifferentTestGroupsInTrialCtis1 = dplyr::case_when(
+      is.na(helper_asNamesPerTestGroupCtis1) ~ NA,
+      helper_numTestGroupsInTrialCtis1 == 1L ~ 1L,
+      .default = sapply(helper_simTestGroupsInTrialCtis1, function(i)
+        max(sum(i < thresholdSimilar), 1L, na.rm = TRUE), simplify = TRUE)
+    ),
+    #
+    #
+    helper_asNamesPerTestGroupCtis2 = lapply(
       authorizedApplication.authorizedPartI.productRoleGroupInfos,
       function(i) {if (any(names(i) == "products"))
         lapply(i[["products"]], "[[", "productName")[
           i[["productRoleName"]] == "Test"] else NA}),
     #
-    helper_simTestGroupsInTrial = lapply(
-      helper_asNamesPerTestGroup, function(i) asTestSimilarityTrial(i)),
+    helper_simTestGroupsInTrialCtis2 = lapply(
+      helper_asNamesPerTestGroupCtis2, function(i) asTestSimilarityTrial(i)),
     #
-    helper_numTestGroupsInTrial = sapply(
-      helper_simTestGroupsInTrial, length, USE.NAMES = FALSE, simplify = TRUE),
+    helper_numTestGroupsInTrialCtis2 = sapply(
+      helper_simTestGroupsInTrialCtis2, length, USE.NAMES = FALSE, simplify = TRUE),
     #
-    analysis_numDifferentTestGroupsInTrial = dplyr::case_when(
-      is.na(helper_asNamesPerTestGroup) ~ NA,
-      helper_numTestGroupsInTrial == 1L ~ 1L,
-      .default = sapply(helper_simTestGroupsInTrial, function(i)
+    analysis_numDifferentTestGroupsInTrialCtis2 = dplyr::case_when(
+      is.na(helper_asNamesPerTestGroupCtis2) ~ NA,
+      helper_numTestGroupsInTrialCtis2 == 1L ~ 1L,
+      .default = sapply(helper_simTestGroupsInTrialCtis2, function(i)
         max(sum(i < thresholdSimilar), 1L, na.rm = TRUE), simplify = TRUE)
     ),
     #
-    out = analysis_numDifferentTestGroupsInTrial
-  ) %>% 
-    dplyr::pull(out) -> df$ctis
-  
-  
+    #
+    out = dplyr::case_when(
+      !is.na(analysis_numDifferentTestGroupsInTrialCtis1) ~
+        analysis_numDifferentTestGroupsInTrialCtis1,
+      !is.na(analysis_numDifferentTestGroupsInTrialCtis2) ~
+        analysis_numDifferentTestGroupsInTrialCtis2,
+      .default = NA
+    )
+  ) %>%
+  dplyr::pull(out) -> df$ctis
+
+
   # merge into vector (ordered factor)
   vct <- as.integer(
     dfMergeVariablesRelevel(
@@ -272,13 +302,13 @@ Returns an integer.
       colnames = names(fldsNeeded)
     )
   )
-  
-  
+
+
   #### checks ####
   stopifnot(is.integer(vct) || all(is.na(vct)))
   stopifnot(length(vct) == nrow(df))
-  
+
   # return
   return(vct)
-  
+
 } # end .numTestArmsSubstances
