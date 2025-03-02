a7dbcacce535ea5509507a814a305a5075b561d3
Ralf Herold <ralf.herold@gmx.net>
2025-03-02 19:34:48 +0100

    improve ctrFindActiveSubstanceSynonyms to be more specific and have less errors

--- R/ctrFindActiveSubstanceSynonyms.R
+++ R/ctrFindActiveSubstanceSynonyms.R
@@ -6,7 +6,8 @@
 #' nonproprietary name (INN), a trade or product name, or a company code(s).
 #' To find likely synonyms, the function retrieves from CTGOV2 the field
 #' protocolSection.armsInterventionsModule.interventions.otherNames.
-#' Note this is not free of error and should be checked manually.
+#' Note this does not seem to be based on choices from a dictionary but
+#' may be manually filled, thus is not free of error and needs to be checked.
 #'
 #' @param activesubstance An active substance, in an atomic character vector
 #'
@@ -25,16 +26,13 @@
 #' \dontrun{
 #'
 #' ctrFindActiveSubstanceSynonyms(activesubstance = "imatinib")
-#' #  [1] "imatinib"                    "Carcemia"                    "Cemivil"
-#' #  [4] "CGP 57148"                   "CGP-57148B"                  "CGP57148B"
-#' #  [7] "Gleevac"                     "gleevec"                     "Gleevec (Imatinib Mesylate)"
-#' # [10] "Glevec"                      "glivec"                      "Imatinib"
-#' # [13] "imatinib mesylate"           "Imatinib-AFT"                "IND # 55666"
-#' # [16] "NSC #716051"                 "NSC-716051"                  "QTI571"
-#' # [19] "ST1571"                      "STI 571"                     "STI-571"
-#' # [22] "STI571"                      "tyrosine kinase inhibitors"
+#' #  [1] "imatinib"          "CGP 57148"         "CGP 57148B"
+#' #  [4] "CGP57148B"         "Gleevec"           "GLIVEC"
+#' #  [7] "Imatinib"          "Imatinib Mesylate" "NSC 716051"
+#' # [10] "ST1571"            "STI 571"           "STI571"
 #'
 #' }
+#'
 ctrFindActiveSubstanceSynonyms <- function(activesubstance = "", verbose = FALSE) {
 
   # check parameters
@@ -78,37 +76,47 @@ ctrFindActiveSubstanceSynonyms <- function(activesubstance = "", verbose = FALSE
     return(NULL)
   }
 
+  # get content
+  jsn <- rawToChar(tmp[["content"]])
+
   # digest results
-  nrec <- jqr::jq(
-    textConnection(rawToChar(tmp[["content"]])),
-    " .studies | length ")
+  nrec <- jqr::jq(textConnection(jsn), " .studies | length ")
 
   # inform user
   if (verbose || nrec == 0L) message(
     nrec, " studies found in CTGOV2 for active substance ", activesubstance)
 
-  # extract otherNames for name
-  asx <- jqr::jq(
-    textConnection(rawToChar(tmp[["content"]])), paste0(
-      ' .studies[] | .protocolSection.armsInterventionsModule.interventions
-      | select ( length > 0 ) | .[]
-      | select ( .name | test("^', activesubstance, '"; "i") and
-                       ( test( ", | or | and | & | [+] " ) | not ) )
-      | .otherNames | select( length > 0 ) | .[] ')
-  )
+  # jsonview::json_tree_view(jsn)
 
-  # prepare and return output
+  # extract otherNames for name
+  asx <- jqr::jq(textConnection(jsn), paste0(
+    ' .studies[] | .protocolSection.armsInterventionsModule.interventions
+    | select ( length > 0 ) | .[]
+    | select ( .name | test("^', activesubstance, '( |$)"; "i") )
+    | .otherNames | select( length > 0 ) | .[]
+  '))
+
+  # cleanup
+
+  # prepare output
   asx <- gsub('"', "", asx)
-  asx <- sort(asx)
   # remove some decorations
   asx <- gsub("@|\U000AE|Trade name: ?| ?[(]?INN[)]?|[(]R[)]", "", asx)
   # some otherNames are multiple active substances
   asx <- asx[!grepl("(,|/| and | or )", asx)]
   # remove descriptive elements
   asx <- asx[!grepl("(intervent|treat|therapy|combin|none)", asx, ignore.case = TRUE)]
+  # normalise a bit
+  asx <- gsub("[-]", " ", asx)
+
+  # remove less frequent occurrences
+  asx <- sort(table(asx))
+  asx <- names(asx[asx > quantile(asx, 2/3)])
+
   # deduplicate irrespective of case
   asx <- asx[!duplicated(tolower(asx))]
   asx <- unique(asx)
+  asx <- sort(asx)
   asx <- c(activesubstance, asx)
 
   # return
