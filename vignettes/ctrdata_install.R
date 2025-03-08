e6b7b86ed8b54fc2c17ab9bbc6611db960d0b385
Ralf Herold <ralf.herold@gmx.net>
2025-03-08 12:49:54 +0100

    change vignettes to html instead of pdf

---
+++ vignettes/ctrdata_install.R
@@ -0,0 +1,14 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(eval = FALSE)
+
+## ----install_ctrdata, eval=FALSE----------------------------------------------
+# install.packages("ctrdata")
+
+## ----eval=FALSE---------------------------------------------------------------
+# # install package under development
+# install.packages(c("remotes"))
+# remotes::install_github("rfhb/ctrdata", dependencies = TRUE, build_vignettes = TRUE)
+
+## ----attach_ctrdata-----------------------------------------------------------
+# library(ctrdata)
+
