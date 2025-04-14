b7d68822ccdd360df43c3a55a6f431c749237203
Ralf Herold <ralf.herold@gmx.net>
2025-04-14 16:47:04 +0200

    adapt tests to ctrGenerateQueries new default, medicine interventional trials

--- inst/tinytest/test_ctrdata_function_ctrgeneratequeries.R
+++ inst/tinytest/test_ctrdata_function_ctrgeneratequeries.R
@@ -24,9 +24,9 @@ tf <- function() {
   # test
   expect_equivalent(c(
     "https://www.clinicaltrialsregister.eu/ctr-search/search?query=cancer AND antibody&dateFrom=2000-01-01&dateTo=2030-01-01&resultsstatus=trials-with-results",
-    "https://www.isrctn.com/search?&filters=condition:cancer,intervention:antibody,GT+overallStartDate:2000-01-01,LE+overallStartDate:2030-01-01,GT+overallEndDate:2000-01-01,LE+overallEndDate:2030-01-01,results:withResults&q=",
-    "https://clinicaltrials.gov/search?cond=cancer&intr=antibody&start=2000-01-01_2030-01-01&primComp=2000-01-01_2030-01-01&aggFilters=results:with",
-    "https://clinicaltrials.gov/expert-search?term=AREA[ConditionSearch]\"cancer\"  AND AREA[InterventionSearch]\"antibody\"  AND AREA[StartDate]RANGE[2000-01-01,2030-01-01]  AND AREA[CompletionDate]RANGE[2000-01-01,2030-01-01] AND (NOT AREA[ResultsFirstPostDate]MISSING) ",
+    "https://www.isrctn.com/search?&q=&filters=condition:cancer,intervention:antibody,GT+overallStartDate:2000-01-01,LE+overallStartDate:2030-01-01,GT+overallEndDate:2000-01-01,LE+overallEndDate:2030-01-01,primaryStudyDesign:Interventional,phase:Phase 0,phase:Phase I,phase:Phase II,phase:Phase III,phase:Phase IV,phase:Phase I/II,phase:Phase II/III,phase:Phase III/IV,results:withResults",
+    "https://clinicaltrials.gov/search?cond=cancer&intr=antibody&start=2000-01-01_2030-01-01&primComp=2000-01-01_2030-01-01&aggFilters=studyType:int,results:with",
+    "https://clinicaltrials.gov/expert-search?term=AREA[ConditionSearch]\"cancer\" AND AREA[InterventionSearch]\"antibody\" AND AREA[StartDate]RANGE[2000-01-01,2030-01-01] AND AREA[CompletionDate]RANGE[2000-01-01,2030-01-01] AND (AREA[StudyType]INTERVENTIONAL) AND (NOT AREA[ResultsFirstPostDate]MISSING)",
     "https://euclinicaltrials.eu/ctis-public/search#searchCriteria={\"medicalCondition\":\"cancer\",\"containAll\":\"antibody\",\"eeaStartDateFrom\":\"2000-01-01\",\"eeaStartDateTo\":\"2030-01-01\",\"eeaEndDateFrom\":\"2000-01-01\",\"eeaEndDateTo\":\"2030-01-01\",\"hasClinicalStudyReport\":true}"
   ), urls)
 
@@ -42,9 +42,9 @@ tf <- function() {
   # test
   expect_equivalent(c(
     "https://www.clinicaltrialsregister.eu/ctr-search/search?query=bispecific&dateTo=2030-01-01",
-    "https://www.isrctn.com/search?&filters=intervention:bispecific,LE+overallStartDate:2030-01-01&q=",
-    "https://clinicaltrials.gov/search?&intr=bispecific&start=_2030-01-01",
-    "https://clinicaltrials.gov/expert-search?term=AREA[InterventionSearch]\"bispecific\"  AREA[StartDate]RANGE[MAX,2030-01-01] ",
+    "https://www.isrctn.com/search?&q=&filters=intervention:bispecific,LE+overallStartDate:2030-01-01,primaryStudyDesign:Interventional,phase:Phase 0,phase:Phase I,phase:Phase II,phase:Phase III,phase:Phase IV,phase:Phase I/II,phase:Phase II/III,phase:Phase III/IV",
+    "https://clinicaltrials.gov/search?&intr=bispecific&start=_2030-01-01&aggFilters=studyType:int",
+    "https://clinicaltrials.gov/expert-search?term=AREA[InterventionSearch]\"bispecific\" AREA[StartDate]RANGE[MAX,2030-01-01] AND (AREA[StudyType]INTERVENTIONAL)",
     "https://euclinicaltrials.eu/ctis-public/search#searchCriteria={\"containAll\":\"bispecific\",\"eeaStartDateTo\":\"2030-01-01\"}"
   ), urls)
 
@@ -60,11 +60,11 @@ tf <- function() {
   # test
   expect_equivalent(c(
     "https://www.clinicaltrialsregister.eu/ctr-search/search?query=cardiac failure",
-    "https://www.isrctn.com/search?&filters=condition:cardiac failure,GT+overallEndDate:2000-01-01&q=",
-    "https://clinicaltrials.gov/search?cond=cardiac failure&primComp=2000-01-01_",
-    "https://clinicaltrials.gov/expert-search?term=AREA[ConditionSearch]\"cardiac failure\"  AND AREA[CompletionDate]RANGE[2000-01-01,MAX] ",
+    "https://www.isrctn.com/search?&q=&filters=condition:cardiac failure,GT+overallEndDate:2000-01-01,primaryStudyDesign:Interventional,phase:Phase 0,phase:Phase I,phase:Phase II,phase:Phase III,phase:Phase IV,phase:Phase I/II,phase:Phase II/III,phase:Phase III/IV",
+    "https://clinicaltrials.gov/search?cond=cardiac failure&primComp=2000-01-01_&aggFilters=studyType:int",
+    "https://clinicaltrials.gov/expert-search?term=AREA[ConditionSearch]\"cardiac failure\" AND AREA[CompletionDate]RANGE[2000-01-01,MAX] AND (AREA[StudyType]INTERVENTIONAL)",
     "https://euclinicaltrials.eu/ctis-public/search#searchCriteria={\"medicalCondition\":\"cardiac failure\",\"eeaEndDateFrom\":\"2000-01-01\"}"
-  ), urls)
+    ), urls)
 
 
   # generate 4
@@ -79,9 +79,9 @@ tf <- function() {
   # test
   expect_equivalent(c(
     "https://www.clinicaltrialsregister.eu/ctr-search/search?query=cancer&status=ongoing&status=trial-now-transitioned&status=suspended-by-ca&status=temporarily-halted&status=restarted&dateFrom=2000-01-01",
-    "https://www.isrctn.com/search?&filters=condition:cancer,trialStatus:ongoing,GT+overallStartDate:2000-01-01&q=",
-    "https://clinicaltrials.gov/search?cond=cancer&start=2000-01-01_&aggFilters=status:act rec",
-    "https://clinicaltrials.gov/expert-search?term=AREA[ConditionSearch]\"cancer\"  AND (AREA[LocationStatus]\"ACTIVE_NOT_RECRUITING\" OR AREA[LocationStatus]\"ENROLLING_BY_INVITATION\" OR AREA[LocationStatus]\"RECRUITING\")  AND AREA[StartDate]RANGE[2000-01-01,MAX] ",
+    "https://www.isrctn.com/search?&q=&filters=condition:cancer,trialStatus:ongoing,GT+overallStartDate:2000-01-01,primaryStudyDesign:Interventional,phase:Phase 0,phase:Phase I,phase:Phase II,phase:Phase III,phase:Phase IV,phase:Phase I/II,phase:Phase II/III,phase:Phase III/IV",
+    "https://clinicaltrials.gov/search?cond=cancer&start=2000-01-01_&aggFilters=status:act rec,studyType:int",
+    "https://clinicaltrials.gov/expert-search?term=AREA[ConditionSearch]\"cancer\" AND (AREA[OverallStatus]\"ACTIVE_NOT_RECRUITING\" OR AREA[OverallStatus]\"ENROLLING_BY_INVITATION\" OR AREA[OverallStatus]\"RECRUITING\") AND AREA[StartDate]RANGE[2000-01-01,MAX] AND (AREA[StudyType]INTERVENTIONAL)",
     "https://euclinicaltrials.eu/ctis-public/search#searchCriteria={\"medicalCondition\":\"cancer\",\"status\":[2,3,4,6,7],\"eeaStartDateFrom\":\"2000-01-01\"}"
   ), urls)
 
@@ -98,11 +98,11 @@ tf <- function() {
   # test
   expect_equivalent(c(
     "https://www.clinicaltrialsregister.eu/ctr-search/search?query=heart failure&phase=phase-two&phase=phase-three&dateFrom=2000-01-01",
-    "https://www.isrctn.com/search?&filters=condition:heart failure,phase:Phase II/III,GT+overallStartDate:2000-01-01&q=",
-    "https://clinicaltrials.gov/search?cond=heart failure&start=2000-01-01_&aggFilters=phase:2 3",
-    "https://clinicaltrials.gov/expert-search?term=AREA[ConditionSearch]\"heart failure\"  AND (AREA[Phase]\"PHASE2\" OR AREA[Phase]\"PHASE3\")  AND AREA[StartDate]RANGE[2000-01-01,MAX] ",
+    "https://www.isrctn.com/search?&q=&filters=condition:heart failure,phase:Phase II/III,GT+overallStartDate:2000-01-01,primaryStudyDesign:Interventional",
+    "https://clinicaltrials.gov/search?cond=heart failure&start=2000-01-01_&aggFilters=phase:2 3,studyType:int",
+    "https://clinicaltrials.gov/expert-search?term=AREA[ConditionSearch]\"heart failure\" AND (AREA[Phase]\"PHASE2\" OR AREA[Phase]\"PHASE3\") AND AREA[StartDate]RANGE[2000-01-01,MAX] AND (AREA[StudyType]INTERVENTIONAL)",
     "https://euclinicaltrials.eu/ctis-public/search#searchCriteria={\"medicalCondition\":\"heart failure\",\"trialPhaseCode\":[10],\"eeaStartDateFrom\":\"2000-01-01\"}"
-  ), urls)
+    ), urls)
 
 
   # generate 6
@@ -119,11 +119,11 @@ tf <- function() {
   # test
   expect_equivalent(c(
     "https://www.clinicaltrialsregister.eu/ctr-search/search?query=heart failure&phase=phase-two&phase=phase-three&age=children&age=adolescent&age=infant-and-toddler&age=newborn&age=preterm-new-born-infants&age=under-18&age=adult&dateFrom=2000-01-01&country=de&country=se",
-    "https://www.isrctn.com/search?&filters=condition:heart failure,phase:Phase II/IIINA,GT+overallStartDate:2000-01-01,recruitmentCountry:Germany,recruitmentCountry:Sweden&q=",
-    "https://clinicaltrials.gov/expert-search?term=AREA[ConditionSearch]\"heart failure\"  AND (AREA[Phase]\"PHASE2\" OR AREA[Phase]\"PHASE3\")  AND (NA)  AND AREA[StartDate]RANGE[2000-01-01,MAX]  AND (AREA[LocationCountry]\"Germany\" OR AREA[LocationCountry]\"Sweden\") ",
-    "https://clinicaltrials.gov/expert-search?term=AREA[ConditionSearch]\"heart failure\"  AND (AREA[Phase]\"PHASE2\" OR AREA[Phase]\"PHASE3\")  AND (NA)  AND AREA[StartDate]RANGE[2000-01-01,MAX]  AND (AREA[LocationCountry]\"Germany\" OR AREA[LocationCountry]\"Sweden\") ",
+    "https://www.isrctn.com/search?&q=&filters=condition:heart failure,phase:Phase II/IIINA,GT+overallStartDate:2000-01-01,primaryStudyDesign:Interventional,recruitmentCountry:Germany,recruitmentCountry:Sweden",
+    "https://clinicaltrials.gov/expert-search?term=AREA[ConditionSearch]\"heart failure\" AND (AREA[Phase]\"PHASE2\" OR AREA[Phase]\"PHASE3\") AND (NA) AND AREA[StartDate]RANGE[2000-01-01,MAX] AND (AREA[StudyType]INTERVENTIONAL) AND (AREA[LocationCountry]\"Germany\" OR AREA[LocationCountry]\"Sweden\")",
+    "https://clinicaltrials.gov/expert-search?term=AREA[ConditionSearch]\"heart failure\" AND (AREA[Phase]\"PHASE2\" OR AREA[Phase]\"PHASE3\") AND (NA) AND AREA[StartDate]RANGE[2000-01-01,MAX] AND (AREA[StudyType]INTERVENTIONAL) AND (AREA[LocationCountry]\"Germany\" OR AREA[LocationCountry]\"Sweden\")",
     "https://euclinicaltrials.eu/ctis-public/search#searchCriteria={\"medicalCondition\":\"heart failure\",\"trialPhaseCode\":[10],\"ageGroupCode\":[2,3],\"eeaStartDateFrom\":\"2000-01-01\",\"msc\":[84,214]}"
-    ), urls)
+   ), urls)
 
 
   # generate 7
@@ -137,14 +137,12 @@ tf <- function() {
   # test
   expect_equivalent(c(
     "https://www.clinicaltrialsregister.eu/ctr-search/search?query=\"antibody\" AND \"covid\"&status=completed",
-    "https://www.isrctn.com/search?q=\"antibody\" AND \"covid\"&filters=trialStatus:completed",
-    "https://clinicaltrials.gov/search?titles=\"antibody\" AND \"covid\"&aggFilters=status:com",
-    "https://clinicaltrials.gov/expert-search?term=(AREA[TitleSearch]\"antibody\" AND AREA[TitleSearch]\"covid\")  AND (AREA[LocationStatus]\"COMPLETED\") ",
+    "https://www.isrctn.com/search?q=\"antibody\" AND \"covid\"&filters=trialStatus:completed,primaryStudyDesign:Interventional,phase:Phase 0,phase:Phase I,phase:Phase II,phase:Phase III,phase:Phase IV,phase:Phase I/II,phase:Phase II/III,phase:Phase III/IV",
+    "https://clinicaltrials.gov/search?titles=\"antibody\" AND \"covid\"&aggFilters=status:com,studyType:int",
+    "https://clinicaltrials.gov/expert-search?term=(AREA[TitleSearch]\"antibody\" AND AREA[TitleSearch]\"covid\") AND (AREA[OverallStatus]\"COMPLETED\") AND (AREA[StudyType]INTERVENTIONAL)",
     "https://euclinicaltrials.eu/ctis-public/search#searchCriteria={\"containAll\":\"antibody, covid\",\"status\":[5,8]}"
   ), urls)
 
-
-  # sapply(urls, ctrOpenSearchPagesInBrowser)
 
   # end tests
 
